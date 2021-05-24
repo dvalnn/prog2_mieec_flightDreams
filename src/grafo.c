a829e3ad061ec485f8abd4000e8e5e0378d84ebe
dvalinn <tiago.andre.amorim@gmail.com>
2021-05-24 19:12:42 +0100

    pequena refatorização no_remove  -- memory leak ainda por resolver

--- src/grafo.c
+++ src/grafo.c
@@ -260,17 +260,7 @@ no_grafo *no_remove(grafo *g, char *cidade) {
     if (pos_para_remover == -1)
         return NULL;
 
-    if (pos_para_remover != (g->tamanho - 1))
-        no_grafo_swap(&g->nos[pos_para_remover], &g->nos[g->tamanho - 1]);
-
-    g->tamanho--;
-    no_grafo *no_para_remover = g->nos[g->tamanho];
-    g->nos[g->tamanho] = NULL;
-    no_grafo **novo_vetor_nos = (no_grafo **)realloc(g->nos, g->tamanho * sizeof(g->nos[0]));
-
-    if (g->tamanho)
-        if (!check_ptr(novo_vetor_nos, REALLOC_ERROR_MSG, "grafo.c - no_remove() - g->nos realloc"))
-            g->nos = novo_vetor_nos;
+    no_grafo *no_para_remover = g->nos[pos_para_remover];
 
     for (int node = 0; node < g->tamanho; node++)
         for (int aresta = 0; aresta < g->nos[node]->tamanho; aresta++)
@@ -278,6 +268,20 @@ no_grafo *no_remove(grafo *g, char *cidade) {
                 aresta_vetor_apaga(g->nos[node], aresta);
                 aresta--;
             }
+
+    if (pos_para_remover != (g->tamanho - 1))
+        for (int pos = pos_para_remover; pos < g->tamanho - 1; pos++)
+            g->nos[pos] = g->nos[pos + 1];
+
+    no_grafo **novo_vetor_nos = (no_grafo **)realloc(g->nos, (g->tamanho - 1) * sizeof(g->nos[0]));
+
+    if (g->tamanho - 1) {
+        if (!check_ptr(novo_vetor_nos, REALLOC_ERROR_MSG, "grafo.c - no_remove() - g->nos realloc")) {
+            g->nos = novo_vetor_nos;
+            g->tamanho--;
+        }
+    } else
+        g->tamanho--;
 
     return no_para_remover;
 }
