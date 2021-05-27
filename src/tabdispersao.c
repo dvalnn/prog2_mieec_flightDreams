6d6609d4445822057c230eed1bdb2dc605b14441
dvalinn <tiago.andre.amorim@gmail.com>
2021-05-27 13:18:00 +0100

    funçao tabela_dispersao tabela_carrega implementada

--- src/tabdispersao.c
+++ src/tabdispersao.c
@@ -100,9 +100,7 @@ int tabela_adiciona(tabela_dispersao *td, no_grafo *entrada) {
 }
 
 int tabela_remove(tabela_dispersao *td, no_grafo *saida) {
-    if (!td || !saida) return -1;
 
-    // ao remover, meter estado a -1
     return -1;
 }
 
@@ -137,7 +135,15 @@ int tabela_existe(tabela_dispersao *td, const char *cidade) {
 
 tabela_dispersao *tabela_carrega(grafo *g, int capacidade) {
     if (!g || !capacidade) return NULL;
-    return NULL;
+
+        tabela_dispersao *td_a_preencher = tabela_nova(capacidade, hash_krm, sond_rh);
+    if (check_ptr(td_a_preencher, TD_CREATION_ERROR_MSG, "tabdispersao.c - tabela_carrega() - td_a_preencher"))
+        return NULL;
+    for (int i = 0; g->tamanho > i; i++)
+        if (tabela_adiciona(td_a_preencher, g->nos[i]) == -1)
+            return NULL;
+
+    return td_a_preencher; 
 }
 
 /*================================================================================*/
