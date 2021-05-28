67a523832bb755082723bae0b798be85210dccd6
dvalinn <tiago.andre.amorim@gmail.com>
2021-05-28 21:51:44 +0100

    st_nova implementada

--- src/stnova.c
+++ src/stnova.c
@@ -59,6 +59,8 @@ int mapa_insere(mapa_destinos *md, no_grafo *origem, int index_ligacao) {
     int index = md->hfunc(ligacao->destino->cidade, md->n_voos);
     ligacao_direta *voo = md->voos[index];
 
+    // printf(" - A inserir: %s, no index %d\n", ligacao->destino->cidade, index);
+    // printf("\t n_voos a partir da origem: %d, origem: %s\n", md->n_voos, origem->cidade);
     while (voo) {
         if (voo->no_destino == ligacao->destino) {
             if (origem->arestas[voo->index_mais_barato]->preco > ligacao->preco)
@@ -186,269 +188,26 @@ int st_importa_grafo(estrutura *st, grafo *g) {
 }
 
 char *st_pesquisa(estrutura *st, char *origem, char *destino) {
-    return NULL;
+    if (!st || !origem || !destino) return NULL;
+
+    int index_origem = (int)st->hfunc(origem, st->n_origens);
+    elemento *pos_atual = st->elementos[index_origem];
+
+    while (pos_atual && strcmp(pos_atual->no_de_origem->cidade, origem))
+        pos_atual = pos_atual->proximo;
+
+    if (!pos_atual || !pos_atual->todos_os_destinos->n_voos) return NULL;
+
+    int index_destino = (int)pos_atual->todos_os_destinos->hfunc(destino, pos_atual->todos_os_destinos->n_voos);
+    ligacao_direta *voo = pos_atual->todos_os_destinos->voos[index_destino];
+
+    while (voo && strcmp(voo->no_destino->cidade, destino))
+        voo = voo->proximo;
+
+    if (!voo) return NULL;
+
+    return pos_atual->no_de_origem->arestas[voo->index_mais_barato]->codigo;
 }
-// tabela_dispersao* tabela_nova(int tamanho, hash_func *hfunc)
-// {
-//     /* aloca memoria para a estrutura tabela_dispersao */
-//     if (tamanho < 1 || hfunc == NULL)
-//         return NULL;
-
-//     tabela_dispersao *t = (tabela_dispersao*) malloc(sizeof (tabela_dispersao));
-//     if (t == NULL)
-//         return NULL;
-
-//     /* aloca memoria para os elementos */
-//     t->elementos = (td_elemento **) calloc(tamanho, sizeof (td_elemento*));
-//     if (t->elementos == NULL) {
-//         free(t);
-//         return NULL;
-//     }
-
-//     t->tamanho = tamanho;
-//     t->hfunc = hfunc;
-
-//     return t;
-// }
-
-// void tabela_apaga(tabela_dispersao *td)
-// {
-//     int i;
-//     td_elemento *elem, *aux;
-
-//     if (td == NULL) return;
-
-//     /* para cada entrada na tabela */
-//     for (i = 0; i < td->tamanho; i++)
-//     {
-//         /* e enquanto existirem elementos nessa entrada */
-//         elem = td->elementos[i];
-//         while (elem != NULL)
-//         {
-//             /* liberta cada elemento */
-//             aux = elem->proximo;
-//             free(elem->obj);
-//             free(elem);
-//             elem = aux;
-//         }
-//     }
-
-//     /* liberta vetor e estrutura */
-//     free(td->elementos);
-//     free(td);
-// }
-
-// int tabela_insere(tabela_dispersao *td, const char *chave, const char *valor)
-// {
-//     int index;
-//     td_elemento * elem;
-
-//     if (!td || !chave || !valor) return TABDISPERSAO_ERRO;
-
-//     /* calcula hash para a string a adicionar */
-//     index = td->hfunc(chave, td->tamanho);
-
-//     /* verifica se chave ja' existe na lista */
-//     elem = td->elementos[index];
-//     while (elem != NULL && strcmp(elem->obj->chave, chave) != 0)
-//         elem = elem->proximo;
-
-//     if (elem == NULL)
-//     {
-//         /* novo elemento, chave nao existe na lista */
-
-//         /* aloca memoria para o elemento */
-//         elem = (td_elemento*) malloc(sizeof (td_elemento));
-//         if (elem == NULL)
-//             return TABDISPERSAO_ERRO;
-
-//         /* aloca memoria para o objeto */
-//         elem->obj = (objeto*) malloc(sizeof (objeto));
-//         if (elem->obj == NULL)
-//             return TABDISPERSAO_ERRO;
-
-//         /* copia chave e valor */
-//         strcpy(elem->obj->chave, chave);
-//         strcpy(elem->obj->valor, valor);
-
-//         /* insere no inicio da lista */
-//         elem->proximo = td->elementos[index];
-//         td->elementos[index] = elem;
-//     } else {
-//         /* chave repetida, simplesmente atualiza o valor */
-//         strcpy(elem->obj->valor, valor);
-//     }
-
-//     return TABDISPERSAO_OK;
-// }
-
-// int tabela_remove(tabela_dispersao *td, const char *chave)
-// {
-//     int index;
-//     td_elemento * elem, * aux;
-
-//     if (!td) return TABDISPERSAO_ERRO;
-
-//     /* calcula hash para a string a adicionar */
-//     index = td->hfunc(chave, td->tamanho);
-
-//     elem = td->elementos[index];
-//     aux = NULL;
-
-//     /* para cada elemento na posicao index */
-//     while(elem != NULL)
-//     {
-//         /* se for a string que se procura, e' removida */
-//         if (strcmp(elem->obj->chave, chave) == 0)
-//         {
-//             /* se nao for a primeira da lista */
-//             if (aux != NULL)
-//                 aux->proximo = elem->proximo;
-//             else
-//                 td->elementos[index] = elem->proximo;
-//             free(elem->obj);
-//             free(elem);
-
-//             return TABDISPERSAO_OK;
-//         }
-
-//         aux = elem;
-//         elem = elem->proximo;
-//     }
-//     return TABDISPERSAO_NAOEXISTE;
-// }
-
-// int tabela_existe(tabela_dispersao *td, const char *chave)
-// {
-//     int index;
-//     td_elemento * elem;
-
-//     if (!chave || !td) return TABDISPERSAO_ERRO;
-
-//     /* calcula hash para a string a adicionar */
-//     index = td->hfunc(chave, td->tamanho);
-
-//     /* percorre lista na posicao index e retorna 1 se encontrar */
-//     elem = td->elementos[index];
-
-//     while(elem != NULL)
-//     {
-//         if (!strcmp(elem->obj->chave, chave))
-//             return TABDISPERSAO_EXISTE;
-//         elem = elem->proximo;
-//     }
-//     return TABDISPERSAO_NAOEXISTE;
-// }
-
-// const char* tabela_valor(tabela_dispersao *td, const char *chave)
-// {
-//     int index;
-//     td_elemento * elem;
-
-//     if (!td) return NULL;
-
-//     /* calcula hash para a string a adicionar */
-//     index = td->hfunc(chave, td->tamanho);
-
-//     /* percorre lista na posicao index e retorna 1 se encontrar */
-//     elem = td->elementos[index];
-
-//     while(elem != NULL)
-//     {
-//         if (!strcmp(elem->obj->chave, chave))
-//             return elem->obj->valor;
-//         elem = elem->proximo;
-//     }
-//     return NULL;
-// }
-
-// int tabela_esvazia(tabela_dispersao *td)
-// {
-//     int i;
-//     td_elemento * elem, * aux;
-
-//     if (!td) return TABDISPERSAO_ERRO;
-
-//     /* para cada entrada na tabela */
-//     for (i = 0; i < td->tamanho; i++)
-//     {
-//         /* apaga todos os elementos da entrada */
-//         elem = td->elementos[i];
-//         while(elem != NULL)
-//         {
-//             aux = elem->proximo;
-//             free(elem->obj);
-//             free(elem);
-//             elem = aux;
-//         }
-//         td->elementos[i] = NULL;
-//     }
-//     return TABDISPERSAO_OK;
-// }
-
-// int tabela_numelementos(tabela_dispersao *td)
-// {
-//     int i, count = 0;
-//     td_elemento * elem;
-
-//     if (!td) return TABDISPERSAO_ERRO;
-
-//     /* percorre todos os elementos da tabela */
-//     for (i = 0; i < td->tamanho; i++)
-//     {
-//         elem = td->elementos[i];
-//         while(elem != NULL)
-//         {
-//             elem = elem->proximo;
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-
-// objeto * tabela_elementos(tabela_dispersao *td, int *n)
-// {
-//     objeto *v;
-//     int i, j;
-//     td_elemento * elem;
-//     objeto * valor;
-
-//     *n = tabela_numelementos(td);
-
-//     if ((*n) <= 0)
-//         return NULL;
-
-//     v = (objeto*) malloc(sizeof(objeto)*(*n));
-//     if (!v)
-//         return NULL;
-
-//     for (i=0,j=0; i < td->tamanho; i++)
-//     {
-//         if (td->elementos[i])
-//         {
-//             elem = td->elementos[i];
-//             while (elem)
-//             {
-//                 valor = elem->obj;
-//                 v[j++] = *valor;
-//                 elem = elem->proximo;
-//             }
-//         }
-//     }
-//     return v;
-// }
-
-// unsigned long hash_krm(const char* chave, int tamanho)
-// {
-//     unsigned long h, i;
-//     h = 7;
-//     i = 0;
-
-//     while(chave[i])
-//         h += chave[i++];
-
-//     return h % tamanho;
-// }
 
 unsigned long hash_djbm(const char *chave, int tamanho) {
     unsigned long h, i;
@@ -463,29 +222,6 @@ unsigned long hash_djbm(const char *chave, int tamanho) {
 
     return h % tamanho;
 }
-
-// /*================================================================================*/
-// void mostraTabela(tabela_dispersao *td)
-// {
-//     int i;
-//     td_elemento * elem;
-//     printf("TABELA DE DISPERSAO (%d elementos)\n", tabela_numelementos(td));
-//     for (i = 0; i < td->tamanho; i++)
-//     {
-//         if (td->elementos[i])
-//         {
-//             printf("\t[%2d]", i);
-//             elem = td->elementos[i];
-//             while (elem)
-//             {
-//                 printf(" : [\"%s\" :: \"%s\"]", elem->obj->chave, elem->obj->valor);
-//                 elem = elem->proximo;
-//             }
-//             printf("\n");
-//         }
-//     }
-//     printf("\n");
-// }
 
 #undef TRUE
 #undef FALSE
